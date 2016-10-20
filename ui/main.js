3f1e179b4334e01248e50d29cdf78144de97f0f5
hitman1996 <tasgavkar21@gmail.com>
2016-10-20 18:29:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,11 @@
 console.log('Loaded!');
 alert('im java script');
 var imag=document.getElementById('imag');
+var marginLeft=0;
+ function moveRight(){
+     marginLeft=marginLeft +10;
+     img.style.marginLeft+'px';
+ }
 img.onclick=function(){
-    
-    
+    var interval=setInterval(moveRight,100);
 }
