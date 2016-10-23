c28937a15fa567d4c628434572b48b95f30cce80
hitman1996 <tasgavkar21@gmail.com>
2016-10-23 12:26:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,6 +78,11 @@ app.get('/:articleName',function(req,res){
     var articleName = req.params.articleName;
    res.send(createTemplate(articles[articleName]));
 });
+var counter=0;
+app.get('/counter', function (req, res) {
+    counter=counter+1;
+  res.sendFile(counter.toString());
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
