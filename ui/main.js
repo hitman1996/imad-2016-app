4024ef9169d52b62b558cb08e5cd5c53ab8fd2e1
hitman1996 <tasgavkar21@gmail.com>
2016-10-23 12:42:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,10 @@
-console.log('Loaded!');
-alert('im java script');
-var imag=document.getElementById('imag');
-var marginLeft=0;
- function moveRight(){
-     marginLeft=marginLeft +10;
-     img.style.marginLeft+'px';
- }
-img.onclick=function(){
-    var interval=setInterval(moveRight,100);
+var buttton=document.getElementById('counter');
+var counter=0;
+button.onClick=function(){
+    //make request to counter end point
+    //capture response and store
+    //render the var in correct span
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counteer.toString();
 }
