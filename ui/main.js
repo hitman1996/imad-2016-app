94d147abf83ad896f30db78037354951fcaa108f
hitman1996 <tasgavkar21@gmail.com>
2016-10-23 13:01:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,20 @@
 var buttton=document.getElementById('counter');
-var counter=0;
 button.onClick=function(){
-    //make request to counter end point
+    //create request object
+    var request=new XMLHttpRequest();
     //capture response and store
+    request.onreadystatechange= function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if (request.status=== 200){
+               var counter= request.responseText;
+                  var span=document.getElementById('count');
+                  span.innerHTML=counter.toString();
+            }
+        }
+    
     //render the var in correct span
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
-};
+    }
+    //make request
+    request.open('GET','http://hitman1996.imad.hasura-app.io/counter',true);
+    request.send(null);
+}
